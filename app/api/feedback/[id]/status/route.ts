5c7fe5e4f6d0c2e451b5654e559268dae39d2365
Rohit sah <1904277@kiit.ac.in>
2026-09-08 10:23:07 +0530

    fix changes

--- app/api/feedback/[id]/status/route.ts
+++ app/api/feedback/[id]/status/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(request: NextRequest, {params}: {params: Promise<{id : number}>}) {
+export async function PATCH(request: NextRequest, {params}: {params: Promise<{id : string}>}) {
     try {
         //fetching userid from clerk - auth()
         const {userId} = await auth();
